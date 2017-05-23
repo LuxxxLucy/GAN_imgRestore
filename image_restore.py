73f915a3d672f171df801a53f205d2dfbc3b7989
陆家林 <3140102299@zju.edu.cn>
2017-05-23 15:14:01 +0800

    initial on opencv as lib

--- image_restore.py
+++ image_restore.py
@@ -1,15 +1,24 @@
 import os,sys
+import cv2
+import numpy as np
+
+from img_utility import *
+
 
 class Image:
     def __init__(self,filename):
         self.filename=filename
-        self.image=open(filename)
+        try:
+            self.image=read_img(filename)
+            show_image(self.image)
+        except:
+            print("sorry, file",filename,"does not exists. please check")
+            quit()
         return
 
-    def open_file(filename):
-        return open(filename,"rb")
-
     def restore(self):
+        self.new_image=np.zeros(self.image.shape,np.uint8)
+        show_image(self.new_image)
         print ("restore!")
 
     def save_as(filename):
